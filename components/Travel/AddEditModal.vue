deab66dc8bc6ac4e441b8149b31fcb533c61261d
Eriona Memko <eriona.memko@abilio.com>
2024-06-12 19:21:20 +0200

    fix: wrong classes

--- components/Travel/AddEditModal.vue
+++ components/Travel/AddEditModal.vue
@@ -1,61 +1,61 @@
 <template>
   <Modal :isOpen="isModalOpen" @close="closeModal">
-    <form @submit.prevent="submitForm">
+    <form class="form-travel" @submit.prevent="submitForm">
       <div class="mb-4">
         <h2 class="text-lg font-bold mb-4">
         {{ isEdit ? "Edit Travel" : "Add Travel" }}
       </h2>
-        <label for="travelTitle" class="travel-modal-label"
+        <label for="travelTitle" class="form-travel-label"
           >Travel Title</label
         >
         <input
           v-model="travelData.travelTitle"
           type="text"
           id="travelTitle"
-          class="travel-modal-input"
+          class="form-travel-input"
           required
         />
       </div>
       <div class="mb-4">
-        <label for="price" class="travel-modal-label"
+        <label for="price" class="form-travel-label"
           >Price</label
         >
         <input
           v-model="travelData.price"
           type="number"
           id="price"
-          class="travel-modal-input"
+          class="form-travel-input"
           required
         />
       </div>
       <div class="mb-4">
         <label
           for="departureDate"
-          class="travel-modal-label"
+          class="form-travel-label"
           >Departure Date</label
         >
         <input
           v-model="travelData.departureDate"
           type="date"
           id="departureDate"
-          class="travel-modal-input"
+          class="form-travel-input"
           required
         />
       </div>
       <div class="mb-4">
-        <label for="returnDate" class="travel-modal-label"
+        <label for="returnDate" class="form-travel-label"
           >Return Date</label
         >
         <input
           v-model="travelData.returnDate"
           type="date"
           id="returnDate"
-          class="travel-modal-input"
+          class="form-travel-input"
           required
         />
       </div>
       <div class="mb-4">
-        <label for="userRating" class="travel-modal-label"
+        <label for="userRating" class="form-travel-label"
           >User Rating</label
         >
         <input
@@ -65,46 +65,46 @@
           min="0"
           max="5"
           id="userRating"
-          class="travel-modal-input"
+          class="form-travel-input"
           required
         />
       </div>
       <div class="mb-4">
-        <label for="description" class="travel-modal-label"
+        <label for="description" class="form-travel-label"
           >Description</label
         >
         <textarea
           v-model="travelData.description"
           id="description"
-          class="travel-modal-input"
+          class="form-travel-input"
           required
         ></textarea>
       </div>
       <div class="mb-4">
-        <label for="location" class="travel-modal-label"
+        <label for="location" class="form-travell-label"
           >Location</label
         >
         <input
           v-model="travelData.location"
           type="text"
           id="location"
-          class="travel-modal-input"
+          class="form-travel-input"
           required
         />
       </div>
       <div class="mb-4">
-        <label for="image" class="travel-modal-label"
+        <label for="image" class="form-travel-label"
           >Image URL</label
         >
         <input
           v-model="travelData.imageUrl"
           type="text"
           id="image"
-          class="travel-modal-input"
+          class="form-travel-input"
           required
         />
       </div>
-      <button class="bg-blue-500 travel-modal-actions" :title="isEdit ? 'Update' : 'Add'" type="submit">
+      <button class="bg-blue-500 form-travel-actions" :title="isEdit ? 'Update' : 'Add'" type="submit">
         {{ isEdit ? "Update" : "Add" }} travel
       </button>
     </form>
@@ -163,7 +163,7 @@ const submitForm = () => {
 </script>
 
 <style lang="postcss" scoped>
-.form {
+.form-travel {
   &-label {
     @apply block text-sm font-medium text-gray-700;
   }
